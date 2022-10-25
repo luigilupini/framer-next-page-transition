e888ba236abdc9c598348b523773042c420efc59
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-25 17:03:18 +0200

    `AnimatePresence` when page unmounts, exit animate

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { AnimatePresence } from "framer-motion";
+/* AnimatePresence: 
+By passing initial={false}, AnimatePresence will disable any initial animations
+on children that are present when the component is first rendered. And `mode` it
+determines how to handle entering and exiting elements.
+
+! For AnimatePresence to work it requires a `key` prop:
+Direct children must each have unique `key` prop so `AnimatePresence` can track
+their presence in the tree, https://www.framer.com/docs/animate-presence.
+*/
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <div className="py-24 px-12 lg:px-48 font-GTWalsheimProBold">
+      <AnimatePresence initial={false} mode={"wait"}>
+        <Navbar />
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default MyApp;
 
 /* Next.js uses the App component to initialize pages.
 `_app.js` contains your whole app, its rendered on any page of the project. You
@@ -13,14 +36,3 @@ can override it and control the page initialization. Which allows you to do:
 
 ! To override the default `App`, create the file `./pages/_app.js`:
 > https://nextjs.org/docs/advanced-features/custom-app */
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <div className="py-24 px-12 lg:px-48 font-GTWalsheimProBold">
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
-  );
-}
-
-export default MyApp;
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,13 @@
 import { motion as m } from "framer-motion";
 export default function Contact() {
   return (
-    <main className="absolute top-0 left-0 w-full h-full text-gray-900 bg-red-400 px-16 lg:px-48">
+    <m.main
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+      className="absolute top-0 left-0 w-full h-full text-gray-900 bg-red-400 px-16 lg:px-48"
+    >
       <div className="my-96 p-1">
         <h1 className="text-6xl text-center lg:text-right lg:text-9xl">
           Let's Talk
@@ -20,6 +26,6 @@ export default function Contact() {
           </ul>
         </div>
       </div>
-    </main>
+    </m.main>
   );
 }
